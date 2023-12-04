1c6bab2e1b3cb0d10d7256a16886f878aa1c5190
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2023-12-05 00:49:01 +0500

    HTML Content can be inserted into MySQL Database now

--- Crawling/crawler.php
+++ Crawling/crawler.php
@@ -1,4 +1,6 @@
 <?php
+include('../Data/CRUD.php');
+
 function crawl_page($url, $depth = 2) {
     static $URLQueue = array();
 
@@ -10,12 +12,16 @@ function crawl_page($url, $depth = 2) {
 
     $dom = new DOMDocument();
     @$dom->loadHTMLFile($url);
+    
+    $htmlContent = $dom->saveHTML();
 
     $anchors = $dom->getElementsByTagName('a');
     foreach ($anchors as $element) {
         $href = $element->getAttribute('href');
         $a_text = $element->nodeValue;
         if (strpos($href, 'http') === 0) {
+            //Insert HTML Content into database
+            InsertHTML($href, $htmlContent);
             // Echo the crawled hyperlinks
             echo "<a href='".$href."'>".$a_text."</a><br>";
             crawl_page($href, $depth - 1);
