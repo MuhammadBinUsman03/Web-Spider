e69a98169da24055ec77c1e634851c6bdde7ba78
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2023-12-05 19:34:45 +0500

    Update schema to include meta info

--- Data/crawlingData.sql
+++ Data/crawlingData.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS crawling;
 USE crawling;
-
 CREATE TABLE crawled_pages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255) NOT NULL,
-    html_content TEXT NOT NULL
+    title VARCHAR(255),
+    html_content TEXT NOT NULL,
+    Description TEXT
 );
