ba9a711c499f3b39d7ede1e64460c0e14f417780
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2023-12-05 14:29:51 +0500

    Better HTML Parsing, And saving search results to DB

--- Crawling/crawler.php
+++ Crawling/crawler.php
@@ -1,10 +1,10 @@
 <?php
-include('../Data/CRUD.php');
+set_time_limit(600);
 
 function crawl_page($url, $depth = 2) {
     static $URLQueue = array();
 
-    if ($depth === 0 || isset($URLQueue[$url])) {
+    if (isset($URLQueue[$url])) {
         return;
     }
 
@@ -13,20 +13,39 @@ function crawl_page($url, $depth = 2) {
     $dom = new DOMDocument();
     @$dom->loadHTMLFile($url);
     
-    $htmlContent = $dom->saveHTML();
 
+    $htmlContent = $dom->saveHTML();
+    InsertHTML($url, $htmlContent);  
+    
+    // If depth has reached end, return because last layer has been inserted into database
+    if ($depth === 0){
+        return;
+    }
     $anchors = $dom->getElementsByTagName('a');
     foreach ($anchors as $element) {
         $href = $element->getAttribute('href');
         $a_text = $element->nodeValue;
-        if (strpos($href, 'http') === 0) {
-            //Insert HTML Content into database
-            InsertHTML($href, $htmlContent);
-            // Echo the crawled hyperlinks
-            echo "<a href='".$href."'>".$a_text."</a><br>";
-            crawl_page($href, $depth - 1);
-        }
+
+        //Convert All relative urls into absolute urls
+        $href = resolve_url($url, $href);
+
+        //Output URL and crawl deeper
+        echo "<a href='".$href."'>".$a_text."</a><br>";
+        crawl_page($href, $depth - 1);
     }
 }
 
+
+
+// appropriate HTML Parsing
+function resolve_url($base, $href) {
+    $url_parts = parse_url($base);
+    if (strpos($href, "//") === 0) {
+        return $url_parts['scheme'] . ":" . $href;
+    } elseif (strpos($href, "/") === 0) {
+        return $url_parts['scheme'] . "://" . $url_parts['host'] . $href;
+    } else {
+        return $base . $href;
+    }
+}
 ?>
